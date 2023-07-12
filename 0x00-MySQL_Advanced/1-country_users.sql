61c65fb2a97106c735595348d8112d096ede0e73
2022phyro <phyrokelstein2@gmail.com>
2023-07-12 20:08:58 +0100

    Found a possible error, Changed 0 and 1 to allow occurence of an empty name

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -3,7 +3,7 @@
 CREATE TABLE IF NOT EXISTS users(
     id INT AUTO_INCREMENT NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255) NOT NULL,
+    name VARCHAR(255),
     country ENUM ('US', 'CO', 'TN') NOT NULL DEFAULT 'US',
     PRIMARY KEY (id)
 );

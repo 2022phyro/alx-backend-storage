ba0d65dbdf4fba5f62c4b27737c9a6fd1429912d
2022phyro <phyrokelstein2@gmail.com>
2023-07-12 20:06:48 +0100

    Debugging 0

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
 -- Intro into specializations and databases. This creates a table of users
 
-CREATE TABLE IF NOT EXISTS users(
+CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255) NOT NULL,

ec17d3710db03fd94b8ff28fc08d101da50aaaab
2022phyro <phyrokelstein2@gmail.com>
2023-07-13 21:14:57 +0100

    Debugged no 13

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -3,41 +3,16 @@ DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUsers;
 DELIMITER $$ ;
 CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
 BEGIN
-    DECLARE avgScore FLOAT;
-    DECLARE t_score FLOAT;
-    DECLARE t_weight FLOAT;
-    DECLARE user_id INT;
-    DECLARE done INT DEFAULT FALSE;
-    DECLARE u_cursor CURSOR FOR
-    SELECT id from users;
-    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
-
-    OPEN u_cursor;
-    user_loop: LOOP
-        FETCH u_cursor INTO user_id;
-
-        -- Calculate total score
-        SELECT SUM((corrections.score * projects.weight)) INTO t_score
-        FROM corrections INNER JOIN projects
-        ON corrections.project_id = projects.id
-        WHERE corrections.user_id = user_id;
-
-        -- Calculate total weight
-        SELECT SUM(projects.weight) INTO t_weight
-        FROM corrections INNER JOIN projects
-        ON corrections.project_id = projects.id
-        WHERE corrections.user_id = user_id;
-
-        SET avgScore = t_score / t_weight;
-        UPDATE users
-        SET average_score = avgScore WHERE id = user_id;
-        FETCH NEXT FROM u_cursor INTO user_id;
-        IF done THEN
-            LEAVE user_loop;
-        END IF;
-    END LOOP user_loop;
-
-    CLOSE u_cursor;
+    UPDATE users,
+    (SELECT users.id, SUM(score * weight) / SUM(weight)
+    AS ave FROM users
+    INNER JOIN corrections
+        ON users.id = corrections.user_id
+    INNER JOIN projects
+        ON projects.id = corrections.project_id
+    GROUP BY users.id) 
+    AS weighted
+    SET users.average_score = weighted.ave
+    WHERE users.id = weighted.id;
 END $$
 DELIMITER ; $$
-

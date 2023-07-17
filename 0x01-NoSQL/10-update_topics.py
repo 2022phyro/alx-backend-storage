bdb3db8e32d5fcd4a78d394db4e048d7a386c7b1
2022phyro <phyrokelstein2@gmail.com>
2023-07-17 13:54:49 -0700

    Attempted no 10

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Updating with pymongo"""
+
+
+def update_topics(mongo_collection, name, topics):
+    criteria = {"name": name}
+    values = {"$set": {"topics": topics}}
+    mongo_collection.update_many(criteria, values)

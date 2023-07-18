486b773b9e70462fdc4db7ecc739e69dea71b157
2022phyro <phyrokelstein2@gmail.com>
2023-07-18 05:39:27 -0700

    Debugging no 12

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -3,6 +3,7 @@
 from pymongo import MongoClient
 
 if __name__ == '__main__':
+    """Attempts to parse the log files"""
     client = MongoClient('mongodb://127.0.0.1:27017')
     logs = client.logs.nginx
     print(logs.count_documents({}), 'logs')
@@ -10,6 +11,6 @@ if __name__ == '__main__':
     print("Methods:")
     for val in options:
         x = logs.count_documents({'method': val})
-        print("    method {}: {:d}".format(val, x))
-    status = logs.count_documents({'path': '/status'})
+        print("\tmethod {}: {:d}".format(val, x))
+    status = logs.count_documents({'path': '/status', 'method': 'GET'})
     print("{} status check".format(status))

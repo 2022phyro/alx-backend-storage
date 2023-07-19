7a21620a1addc707b0ab184beef9c6db9ffc5245
2022phyro <phyrokelstein2@gmail.com>
2023-07-19 13:31:07 +0000

    attempted no 1 and debugged no 0

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Introductionm to redis"""
-from typing import Any
+from typing import Union, Optional, Callable
 import redis
 from uuid import uuid4
 
@@ -13,8 +13,25 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Any) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """Stores a random set of data"""
         key = str(uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> Union[
+            str, bytes, int, float]:
+        """Retrieves an item from redis"""
+        val = self._redis.get(key)
+        sol = val
+        if val and fn:
+            sol = fn(val)
+        return sol
+
+    def get_str(self, key: str) -> str:
+        """Gets a string"""
+        return self.get(key, str)
+
+    def get_int(self, key: str) -> int:
+        """Gets an int """
+        return self.get(key, int)
